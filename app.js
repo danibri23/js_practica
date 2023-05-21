d8a22f1ec8d80bef84088977e9dc7d66ca303399
Daniel <carlosbrizuelamereles@gmail.com>
2023-05-21 19:55:31 -0400

    template listo

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+
 document.addEventListener("DOMContentLoaded", () => {
   datosVenta();
 });
@@ -6,8 +10,30 @@ const datosVenta = async () => {
   try {
     const res = await fetch("api.json");
     const data = await res.json();
-    console.log(data);
+
+    // console.log(data);
+
+    pintarCards(data);
   } catch (error) {
     console.log(error);
   }
+};
+
+const pintarCards = (data) => {
+  data.forEach((producto) => {
+    // console.log(producto);
+
+    templateCard.querySelector("h5").textContent = producto.title;
+    templateCard.querySelector("p").textContent = producto.precio;
+    templateCard
+      .querySelector("img")
+      .setAttribute("src", producto.thumbnailUrl);
+
+    templateCard.querySelector("img").setAttribute("alt", producto.title);
+    templateCard.querySelector("button").dataset.id = producto.id;
+
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  items.appendChild(fragment);
 };
